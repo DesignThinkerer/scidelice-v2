e5ab12a1aebf8ccf2df067cd8f263733e16954c1
TheophileDesmedt <theophiledesmedt@gmail.com>
2023-06-06 14:49:23 +0200

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,11 @@ export const routes: Routes = [
     path: ':id',
     loadComponent: () =>
       import('./page.template/page.template').then((m) => m.PageTemplate),
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then( m => m.HomePage)
   }
+
 
 ];
