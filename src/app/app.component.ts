32ac2119dfddb9c381dbe960bb3de3e8041700bf
TheophileDesmedt <theophiledesmedt@gmail.com>
2023-06-07 00:11:57 +0200

    commented out pages that are not installed yet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,15 +13,15 @@ export class AppComponent {
   public appPages = [
     { title: 'Home', url: '/home', icon: 'home' },
     // { title: 'Profile', url: '/profile', icon: 'person' },
-    { title: 'Login', url: '/login', icon: 'log-in' },
-    { title: 'How', url: '/how', icon: 'flask' },
-    { title: 'Why', url: '/why', icon: 'school' },
-    { title: 'Pantry', url: '/pantry', icon: 'snow' },
-    { title: 'Planner', url: '/planner', icon: 'calendar' },
+    // { title: 'How', url: '/how', icon: 'flask' },
+    // { title: 'Why', url: '/why', icon: 'school' },
+    // { title: 'Pantry', url: '/pantry', icon: 'snow' },
+    // { title: 'Planner', url: '/planner', icon: 'calendar' },
     { title: 'Recipes', url: '/recipes', icon: 'journal' },
-    { title: 'Settings', url: '/settings', icon: 'settings' },
-    { title: 'Success', url: '/success', icon: 'medal' },
-    { title: 'Tools', url: '/tools', icon: 'calculator' },
+    // { title: 'Settings', url: '/settings', icon: 'settings' },
+    // { title: 'Success', url: '/success', icon: 'medal' },
+    // { title: 'Tools', url: '/tools', icon: 'calculator' },
+    { title: 'Login', url: '/login', icon: 'log-in' },
   ];
   constructor() {}
 }
