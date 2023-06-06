c67b95543f099cee012b964210e558ae6f2b1716
TheophileDesmedt <theophiledesmedt@gmail.com>
2023-06-06 17:55:09 +0200

    create home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,10 +9,18 @@ export const routes: Routes = [
   {
     path: 'loader',
     loadComponent: () => import('./pages/loader/loader.page').then( m => m.LoaderPage)
-  },  {
+  },
+  {
     path: 'login',
     loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
+  },
+  {
+    path: 'hom',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
   }
-
 
 ];
