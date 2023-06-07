45072452ccc3f0abf76d34e28eed3bd867009393
TheophileDesmedt <theophiledesmedt@gmail.com>
2023-06-07 10:32:17 +0200

    some tweak to the recipe card component

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { RecipeCardComponent } from 'src/app/components/recipe-card/recipe-card.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, RecipeCardComponent]
 })
 export class HomePage implements OnInit {
 
