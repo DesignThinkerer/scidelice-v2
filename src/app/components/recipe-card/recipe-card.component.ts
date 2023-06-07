cfa9bd441e9523f2da2907129c79e91f80c597b7
TheophileDesmedt <theophiledesmedt@gmail.com>
2023-06-07 09:57:56 +0200

    add recipe card component to the recipes page

--- src/app/components/recipe-card/recipe-card.component.ts
+++ src/app/components/recipe-card/recipe-card.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IonicModule } from '@ionic/angular';
 @Component({
   selector: 'app-recipe-card',
   templateUrl: './recipe-card.component.html',
   styleUrls: ['./recipe-card.component.scss'],
+  standalone: true,
+  imports: [IonicModule]
 })
 export class RecipeCardComponent  implements OnInit {
 
--- src/app/pages/recipes/recipes.page.ts
+++ src/app/pages/recipes/recipes.page.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { RecipeCardComponent } from 'src/app/components/recipe-card/recipe-card.component';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, RecipeCardComponent]
 })
 export class RecipesPage implements OnInit {
 
