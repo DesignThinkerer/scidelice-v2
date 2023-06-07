61fd0d21e9402401dea9e6e680af5bf6fa5bfb2c
TheophileDesmedt <theophiledesmedt@gmail.com>
2023-06-07 10:58:33 +0200

    added ng if to recipe card component

--- src/app/components/recipe-card/recipe-card.component.ts
+++ src/app/components/recipe-card/recipe-card.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common'; // required for ngIf
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 @Component({
@@ -5,10 +6,11 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: './recipe-card.component.html',
   styleUrls: ['./recipe-card.component.scss'],
   standalone: true,
-  imports: [IonicModule]
+  imports: [IonicModule, CommonModule]
 })
 export class RecipeCardComponent  implements OnInit {
-
+  // declaring attributes to control the component
+  hasNotes: boolean = true;
   constructor() { }
 
   ngOnInit() {}
